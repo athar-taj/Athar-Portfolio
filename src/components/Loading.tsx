ae97483839f7629b6957d181385ec1c71036b708
athar-taj <mrtaj2372@gmail.com>
2026-04-10 17:24:17 +0530

    Change Loading line

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -62,8 +62,7 @@ const Loading = ({ percent }: { percent: number }) => {
       <div className="loading-screen">
         <div className="loading-marquee">
           <Marquee>
-            <span> Full Stack Developer</span> <span>Software Engineer</span>
-            <span> Full Stack Developer</span> <span>Software Engineer</span>
+            <span> Software Developer</span>
           </Marquee>
         </div>
         <div
